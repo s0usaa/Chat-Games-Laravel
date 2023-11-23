3966911cc4c4b828000197ad92a182af7d57b552
s0usaa <sousavlc86@gmail.com>
2023-11-23 17:07:21 +0100

    fix: Eliminacion de la relacion con las tablas Users y Games

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,14 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->date('birth_date');
+
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')
+            ->references('id')
+            ->on('roles')
+            ->onUpdate('cascade')
+            ->onDelete('cascade');
+            
             $table->rememberToken();
             $table->timestamps();
         });
