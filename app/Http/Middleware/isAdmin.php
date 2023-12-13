2eb5fbc62ddddc20bc1eb9023b372de4012e8b3f
s0usaa <sousavlc86@gmail.com>
2023-12-13 10:32:26 +0100

    feat: Middleware de Admin creado

--- app/Http/Middleware/isAdmin.php
+++ app/Http/Middleware/isAdmin.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -16,7 +17,16 @@ class isAdmin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        Log::info("Esto es el middleware de Admin");
+        Log::info("Middleware de Admin");
+        $userId = auth()->user()->id;
+        $user = User::find($userId);
+        $role = $user->role_id;
+        if($role != 1) {
+            return response()->json([
+                "success" => false,
+                "message" => "No tienes poder aqui"
+            ]);
+        }
         return $next($request);
     }
 }
